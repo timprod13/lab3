be95b9cf1a8e55fd92707acb31dac8c1c77bf59e
Toshiba_TimP <Toshiba_TimP@192.168.56.1>
2015-05-07 20:02:33 +0700

    инициализация проекта

---
+++ src/newpackage/Suit.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package newpackage;
+
+/**
+ *
+ * @author Toshiba_TimP
+ */
+public class Suit {
+    private String name;
+    
+    public Suit(String name){
+      this.name = name;
+    }
+    
+    public String getName (){
+        return "";
+    }
+    
+}
+
+
