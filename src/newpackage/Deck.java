f02e6ca8c973a82d0202b75abb7cd92f9ac360e0
Toshiba_TimP <Toshiba_TimP@192.168.56.1>
2015-05-11 00:54:33 +0700

    Реализация DeckStack

---
+++ src/newpackage/Deck.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package newpackage;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author Toshiba_TimP
+ */
+public class Deck {
+    private Stack <Card> stack;
+    
+    public Deck() {
+        stack = new Stack<>();
+    }
+}
