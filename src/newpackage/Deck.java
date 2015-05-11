afaac75c90d8c1c5cea6a2a48ad20369654774cb
Toshiba_TimP <Toshiba_TimP@192.168.56.1>
2015-05-11 10:33:37 +0700

    Реализация и генерация DeckStack

--- src/newpackage/Deck.java
+++ src/newpackage/Deck.java
@@ -6,6 +6,7 @@
 
 package newpackage;
 
+import java.util.Random;
 import java.util.Stack;
 
 /**
@@ -14,6 +15,29 @@ import java.util.Stack;
  */
 public class Deck {
     private Stack <Card> stack;
+    private int countshake=100;
+    public void generate(){
+        String[] cardname = {"6","7","8","9","10","Валет","Дама","Король","Туз"};
+        String[] suitname = {"Черви","Бубны","Трефы","Крести"};
+        Random rand = new Random();
+        for(int i=0;i<4;i++){
+            for(int j=0;j<9;j++){
+                stack.add(new Card(cardname[j], new Suit(suitname[i])));
+            }
+        }
+        for (int i=0;i<countshake;i++){
+            int index1 = rand.nextInt(stack.size());
+            int index2 = rand.nextInt(stack.size());
+            if(index1!=index2) {
+                Card a1 = stack.get(index1);
+                stack.set(index1,stack.get(index2));
+                stack.set(index2,a1);
+            }
+        }
+        for(int i=0;i<36;i++){
+        stack.get(i);
+        System.out.println(stack.get(i).getName() +" "+ stack.get(i).getSuitName());}
+    }
     
     public Deck() {
         stack = new Stack<>();
---
+++ test/newpackage/DeckTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package newpackage;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Toshiba_TimP
+ */
+public class DeckTest {
+    
+   @Test
+   public void DeckTest(){
+       Deck deck = new Deck();
+       deck.generate();
+    }
+}
